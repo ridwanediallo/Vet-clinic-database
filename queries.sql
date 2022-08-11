c7dbb0ddfce4adbb7f9120659a85c2a3d860700d
ridwan <ridwanediallo99@gmail.com>
2022-08-11 10:04:41 +0000

    Update commands indentations

--- queries.sql
+++ queries.sql
@@ -43,16 +43,34 @@ SELECT * FROM animals JOIN species ON animals.species_id = species.species_id WH
 SELECT * FROM animals LEFT JOIN owners ON animals.owner_id = owners.owner_id;
 
 -- How many animals are there per species?------------------------------------------------
-SELECT species.name, COUNT(*) FROM animals INNER JOIN species ON animals.species_id = species.species_id GROUP BY species.species_id;
+SELECT species.name, COUNT(*)
+FROM animals
+INNER JOIN species
+ ON animals.species_id = species.species_id
+GROUP BY species.species_id;
 
 -- List all Digimon owned by Jennifer Orwell. --------------------------------------------
-SELECT a.name, o.full_name, s.name FROM animals a INNER JOIN owners o  ON a.owner_id = o.owner_id INNER JOIN species s ON a.species_id = s.species_id
+SELECT a.name, o.full_name, s.name
+FROM animals a
+INNER JOIN owners o
+ ON a.owner_id = o.owner_id
+INNER JOIN species s
+ ON a.species_id = s.species_id
 WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
 
 -- List all animals owned by Dean Winchester that haven't tried to escape --------------------------------------------
-SELECT a.name, o.full_name FROM animals a INNER JOIN owners o ON a.owner_id = o.owner_id
+SELECT a.name, o.full_name
+FROM animals a
+INNER JOIN owners o
+ ON a.owner_id = o.owner_id
 WHERE a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
 
 -- Who owns the most animals? -------------------------------------------------------------------------------
-SELECT COUNT(o.full_name) AS num, o.full_name FROM animals a INNER JOIN owners o ON a.owner_id = o.owner_id GROUP BY o.full_name
+SELECT COUNT(o.full_name) AS num, o.full_name
+FROM animals a
+INNER JOIN owners o
+ ON a.owner_id = o.owner_id
+GROUP BY o.full_name
 ORDER BY num DESC LIMIT 1;
+
+
